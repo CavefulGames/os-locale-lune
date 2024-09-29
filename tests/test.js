f83530d178f339f748fc403ab423670c7ab2368c
kimpure <gimleni593@gmail.com>
2024-09-29 16:03:46 +0900

    bugfix

--- tests/test.js
+++ tests/test.js
@@ -2,3 +2,16 @@ import {osLocale} from 'os-locale';
 
 console.log(await osLocale());
 //=> 'en-US'
+
+function normalise(input) {
+	return input.replace(/_/, '-');
+}
+
+function getLocale(string) {
+	return (string && string.replace(/[.:].*/, ''));
+}
+let r = getLocale(process.env.LANG)
+console.log(r);
+
+// console.log(normalise());
+
