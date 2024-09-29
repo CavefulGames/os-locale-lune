144435e1807459282f3f87933995ca9905d9e328
kimpure <gimleni593@gmail.com>
2024-09-29 16:11:01 +0900

    readme

---
+++ README.md
@@ -0,0 +1,16 @@
+# os-locale-lune
+
+## Install
+```
+git submodule add submodule/os-locale-lune
+```
+
+## example
+```lua
+--!strict
+local osLocale = require'path'
+print(osLocale())
+--// ko-KR
+```
+
+## note by os-[locale](https://github.com/sindresorhus/os-locale)
--- tests/test.js
+++ tests/test.js
@@ -1,17 +1,4 @@
 import {osLocale} from 'os-locale';
 
 console.log(await osLocale());
-//=> 'en-US'
-
-function normalise(input) {
-	return input.replace(/_/, '-');
-}
-
-function getLocale(string) {
-	return (string && string.replace(/[.:].*/, ''));
-}
-let r = getLocale(process.env.LANG)
-console.log(r);
-
-// console.log(normalise());
-
+//=> ko-KR
